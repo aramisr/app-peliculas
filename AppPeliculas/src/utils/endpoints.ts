e7b548cddaa91eab6ddba1d2f01c3182384dd52b
Marcelo Serpas <marcelo.aramis91@gmail.com>
2025-10-22 14:37:53 -0600

    Ajustar rutas de mis endpoints

--- AppPeliculas/src/utils/endpoints.ts
+++ AppPeliculas/src/utils/endpoints.ts
@@ -1,22 +1,21 @@
 const apiURL = import.meta.env.VITE_API_URL;
 
-console.log("API URL:", import.meta.env.VITE_API_URL);
-
 export const endpoints = {
-    generos: {
-        base: `${apiURL}/generos`,
-        get: `${apiURL}/GetGeneros`,
-        getById: (id: number) => `${apiURL}/GetById/${id}`,
-        create: `${apiURL}/AddGenero`,
-        update: (id: number) => `${apiURL}/UpdateGeneros/${id}`,
-        delete: (id: number) => `${apiURL}/DeleteGeneros/${id}`
-    },
-    actores: {
-        base: `${apiURL}/actores`,
-        get: `${apiURL}/GetActores`,
-        getById: (id: number) => `${apiURL}/GetById/${id}`,
-        create: `${apiURL}/AddActor`,
-        update: (id: number) => `${apiURL}/UpdateActores/${id}`,
-        delete: (id: number) => `${apiURL}/DeleteActores/${id}`
-    }
+  generos: {
+    base: `${apiURL}/generos`,
+    get: `${apiURL}/generos/GetGeneros`,
+    getById: (id: number) => `${apiURL}/generos/GetById/${id}`,
+    create: `${apiURL}/generos/AddGenero`,
+    update: (id: number) => `${apiURL}/generos/UpdateGeneros/${id}`,
+    delete: (id: number) => `${apiURL}/generos/DeleteGeneros/${id}`
+  },
+  actores: {
+    base: `${apiURL}/actores`,
+    get: `${apiURL}/actores/GetActores`,
+    getById: (id: number) => `${apiURL}/actores/GetById/${id}`,
+    create: `${apiURL}/actores/AddActor`,
+    update: (id: number) => `${apiURL}/actores/UpdateActores/${id}`,
+    delete: (id: number) => `${apiURL}/actores/DeleteActores/${id}`
+  }
 };
+
